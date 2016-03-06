c14d83eea78b321d5e68199e3d2ead0166f49f42
Vishal Chaganti <chaganti_v@yahoo.com>
2016-03-06 17:30:01 -0600

    Implemented Acronyms and initialisms search.

---
+++ Acronyms/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Acronyms
+//
+//  Created by Vishal Chaganti on 3/6/16.
+//  Copyright © 2016 Vishal Chaganti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+pod 'AFNetworking', '~> 3.0'
+pod 'MBProgressHUD', '~> 0.9.2'
+
+target 'Acronyms' do
+
+end
+
